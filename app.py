7c8ad187a8b30a0d6cacc0ba3c03f9348cb4beb8
Rajkumar. S <myidrajkumar@gmail.com>
2024-09-14 17:55:52 +0530

    Just added the coments and packages

--- app.py
+++ app.py
@@ -1,59 +1,70 @@
-from flask import Flask, render_template, request, jsonify
+"""Main Application"""
+
 import uuid
+
+from flask import Flask, jsonify, render_template, request
+
 from answers_handling import answer_questions
 from send_mail import send_email
 from vector_stores import get_session_history
 
-
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    """Main Page"""
+    return render_template("index.html")
 
-@app.route('/token', methods=['GET'])
+
+@app.route("/token", methods=["GET"])
 def get_token():
+    """Retrieve Token"""
     session_id = str(uuid.uuid4())
-    return jsonify({'token': session_id})
+    return jsonify({"token": session_id})
 
-@app.route('/ask', methods=['POST'])
+
+@app.route("/ask", methods=["POST"])
 def ask():
+    """Asking the question"""
     data = request.json
-    field = data.get('field')
-    question = data.get('question')
-    session_id = data.get('token')
-    department = data.get('department')
-    
-    if field in ['1', '2', '3']:
+    field = data.get("field")
+    question = data.get("question")
+    session_id = data.get("token")
+    department = data.get("department")
+
+    if field in ["1", "2", "3"]:
         answer = answer_questions(session_id, question, department)
     else:
         answer = "Invalid field selected."
-    
-    return jsonify({'content': answer})
+
+    return jsonify({"content": answer})
 
 
-@app.route('/raiseconcernmail', methods=['POST'])
+@app.route("/raiseconcernmail", methods=["POST"])
 def raise_concern():
+    """Sending concern"""
     try:
         data = request.json
-        department = data.get('department')
-        session_id = data.get('token')
-        sender = 'admin@techinterrupt.com'
-        if department == 'Finance':
-            receiver = 'finance@techinterrupt.com'
-            subject = 'Finance Query'
-        elif department == 'Human Resources':
-            receiver = 'hr@techinterrupt.com'
-            subject = 'HR Query'
-        elif department == 'Operations':
-            receiver = 'operations@techinterrupt.com'
-            subject = 'Operations Query'
+        department = data.get("department")
+        session_id = data.get("token")
+        sender = "admin@techinterrupt.com"
+        if department == "Finance":
+            receiver = "finance@techinterrupt.com"
+            subject = "Finance Query"
+        elif department == "Human Resources":
+            receiver = "hr@techinterrupt.com"
+            subject = "HR Query"
+        elif department == "Operations":
+            receiver = "operations@techinterrupt.com"
+            subject = "Operations Query"
         send_email(sender, receiver, subject, get_session_history(session_id))
-        return jsonify({'success': True, 'message': 'Concern raised successfully'})
+        return jsonify({"success": True, "message": "Concern raised successfully"})
 
     except Exception as e:
         print(f"Error: {e}")
-        return jsonify({'success': False, 'message': 'An error occurred'}), 500
+        return jsonify({"success": False, "message": "An error occurred"}), 500
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
